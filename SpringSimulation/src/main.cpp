8745abe813a7633a8f88e9d327e915eec96b556f
Filipe Alves <filipealves2006@icloud.com>
2026-01-11 13:29:59 +0100

    Created MazeSolver

---
+++ SpringSimulation/src/main.cpp
@@ -0,0 +1,29 @@
+#include <SFML/Graphics/CircleShape.hpp>
+#include <SFML/Graphics/Color.hpp>
+#include <SFML/Graphics/PrimitiveType.hpp>
+#include <SFML/Graphics/RectangleShape.hpp>
+#include <SFML/Graphics/RenderWindow.hpp>
+#include <SFML/Graphics/VertexArray.hpp>
+#include <SFML/System/Vector2.hpp>
+#include <SFML/Window/VideoMode.hpp>
+
+int main() {
+  srand(time(nullptr));
+
+  sf::RenderWindow window(sf::VideoMode({500, 500}), "Maze Generator");
+
+  window.setPosition({1920 - (int)window.getSize().x - 50, 50});
+
+  while (window.isOpen()) {
+    while (const std::optional event = window.pollEvent()) {
+      if (event->is<sf::Event::Closed>()) {
+        window.close();
+      } else if (const auto *keyPressed =
+                     event->getIf<sf::Event::KeyPressed>()) {
+        if (keyPressed->scancode == sf::Keyboard::Scancode::Escape) {
+          window.close();
+        }
+      }
+    }
+  }
+}
